7a5d81d7bedd5916cdd972be0a85065a4ae227cd
Stanley Sung <stanleysung99@gmail.com>
2011-10-20 00:16:14 +0800

    Added C#.NET API

---
+++ cs/Example/Example.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Subuno;
+
+namespace Example
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, string>();
+
+            data.Add("t_id", "7d3n89wn");
+            data.Add("ip_addr", "24.24.24.24");
+            data.Add("customer_name", "John Doe");
+            data.Add("phone", "212-456-7890");
+            data.Add("email", "john.doe@domain.com");
+            data.Add("company", "Doe LLC");
+            data.Add("price", "50.0");
+            data.Add("bin", "480128");
+
+            data.Add("bill_street1", "12 East 71th St");
+            data.Add("bill_street2", "#12");
+            data.Add("bill_city", "New York");
+            data.Add("bill_state", "NY");
+            data.Add("bill_country", "US");
+            data.Add("bill_zip", "10021");
+
+            data.Add("ship_street1", "12 East 71th St");
+            data.Add("ship_street2", "#12");
+            data.Add("ship_city", "New York");
+            data.Add("ship_state", "NY");
+            data.Add("ship_country", "US");
+            data.Add("ship_zip", "10021");
+
+            data.Add("avs_response", "X");
+            data.Add("ccv_response", "M");
+            data.Add("custom1", "first custom value");
+            data.Add("custom2", "second custom value");
+            data.Add("custom3", "third custom value");
+            data.Add("issuer_phone", "18667750556");
+            data.Add("source", "affiliate_code_here");
+
+            try
+            {
+                string result = SubunoApi.Run(
+                    // apikey
+                    "2g4g747g843",
+                    // data
+                    data
+                );
+
+                // result is a string in JSON format with keys/value pairs with data returned by api.
+                Console.WriteLine(result);
+
+            }
+            catch (SubunoApiError e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ cs/Subuno/SubunoApi.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Subuno
+{
+    public class SubunoApi
+    {
+        public static string SUBUNO_SERVER_URI = "https://api.subuno.com/v1/";
+
+        private static string _apikey;
+        private static string _server_uri;
+
+        public static string Run(string apikey, Dictionary<string, string> data)
+        {
+            return Run(apikey, data, SUBUNO_SERVER_URI);
+        }
+
+        public static string Run(string apikey, Dictionary<string, string> data, string serverUri)
+        {
+            SetAuthenticationInfo(apikey, serverUri);
+            return CallServer(data);
+        }
+
+        private static string CallServer(IDictionary<string, string> data)
+        {
+            if (string.IsNullOrEmpty(_apikey))
+            {
+                throw new SubunoApiError("API key not set.");
+            }
+
+            HttpWebRequest request;
+            HttpWebResponse response = null;
+            StreamReader reader;
+            StringBuilder sbSource;
+
+            //add apikey to the data packet.
+            data.Add("apikey", _apikey);
+
+            // serialize data.
+            string urlencoding = string.Empty;
+            urlencoding = data.Aggregate(urlencoding, (current, pair) => current + "&" + HttpUtility.UrlEncode(pair.Key, Encoding.GetEncoding("UTF-8")) + "=" + HttpUtility.UrlEncode(pair.Value, Encoding.GetEncoding("UTF-8")));
+            urlencoding = urlencoding.Substring(1); // remove first &
+
+            UriBuilder builder = null;
+            try
+            {
+                builder = new UriBuilder(new Uri(_server_uri + "?" + urlencoding));
+            }
+            catch (UriFormatException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                // Create and initialize the web request  
+                request = WebRequest.Create(builder.ToString()) as HttpWebRequest;
+
+                // Get response  
+                response = request.GetResponse() as HttpWebResponse;
+
+                if (request.HaveResponse && response != null)
+                {
+                    // Get the response stream  
+                    reader = new StreamReader(response.GetResponseStream());
+
+                    // Read it into a StringBuilder  
+                    sbSource = new StringBuilder(reader.ReadToEnd());
+
+                    // Console application output  
+                    return sbSource.ToString();
+                }
+            }
+            catch (WebException wex)
+            {
+                // This exception will be raised if the server didn't return 200 - OK  
+                // Try to retrieve more information about the network error  
+                if (wex.Response != null)
+                {
+                    using (HttpWebResponse errorResponse = (HttpWebResponse)wex.Response)
+                    {
+                        Console.WriteLine(
+                            "The server returned '{0}' with the status code {1} ({2:d}).",
+                            errorResponse.StatusDescription, errorResponse.StatusCode,
+                            errorResponse.StatusCode);
+                    }
+                }
+            }
+            finally
+            {
+                if (response != null) { response.Close(); }
+            }
+
+            return string.Empty;
+        }
+
+        private static void SetAuthenticationInfo(string apikey, string serverUri)
+        {
+            _apikey = apikey;
+            _server_uri = serverUri;
+        }
+    }
+
+    public class SubunoApiError : Exception
+    {
+        public SubunoApiError(string message) : base(message) { }
+    }
+}
